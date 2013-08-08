40c46d1923bf1157ab434910ca3b6d926ec2b08a
idafereyes <idafe.reyes@servexternos.isban.es>
2013-08-08 09:38:39 +0200

    Se cambia la respuesta del web service: el nombre de los eventos tras SoapFaultClientException se coge de FaultString y se adjunta la respuesta como atributo del evento success.

--- Sample_Application/src/main/java/org/isb/ivr/flow/webservices/BindingWebService.java
+++ Sample_Application/src/main/java/org/isb/ivr/flow/webservices/BindingWebService.java
@@ -1,9 +1,13 @@
 package org.isb.ivr.flow.webservices;
 
 import java.rmi.MarshalException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.isb.ivr.flow.webservices.jaxbbeans.binding.Op1;
 import org.isb.ivr.flow.webservices.jaxbbeans.binding.Op1Response;
+import org.springframework.webflow.core.collection.AttributeMap;
+import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.execution.Event;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.SoapFaultClientException;
@@ -13,22 +17,23 @@ public class BindingWebService extends WebServiceGatewaySupport {
 	public Event op1(Op1 op1) throws Exception, MarshalException{
 		
 		Op1Response response;
-		
+				
 		try{
 			response = (Op1Response)getWebServiceTemplate().marshalSendAndReceive(op1);
 		} catch (SoapFaultClientException e){
 			
-			if ("01".equalsIgnoreCase(e.getSoapFault().getFaultCode().toString())){
-				return new Event(this, "ExNoAccount");
-			}
-			throw new Exception("Soap Fault Exception capturada ");
+			return new Event(this, e.getSoapFault().getFaultStringOrReason());
 			
 		} catch (Exception e){
 			e.printStackTrace();
 			throw new Exception("Error recogiendo resultado del web service" );
 		}
-					
-		return new Event(this, "OK");
+		
+		Map<String,Op1Response> map = new HashMap<String,Op1Response>();
+		map.put("response", response);
+		AttributeMap attributeMap = new LocalAttributeMap(map);
+		
+		return new Event(this, "success", attributeMap);
 	}
 
 
