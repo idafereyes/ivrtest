8c0b72b869d4d0a86ed50f2aceab6bf9aad1478e
idafereyes <idafe.reyes@servexternos.isban.es>
2013-09-04 12:27:41 +0200

    Implementación del elemento Transfer y del bean TransferResultInfo.

--- jvoiceframework/jvoiceframework-core/src/main/java/com/vectorsf/jvoiceframework/core/bean/AppConfigDefaults.java
+++ jvoiceframework/jvoiceframework-core/src/main/java/com/vectorsf/jvoiceframework/core/bean/AppConfigDefaults.java
@@ -27,6 +27,21 @@ public class AppConfigDefaults implements Serializable {
 	//Está pendiente ver de qué manera toman valor estos atributos.
 	@Value("false")
 	private boolean catchHangup;
+	
+	//TODO En un primer momento le estamos asignando un valor hardcodeado.
+	//Está pendiente ver de qué manera toman valor estos atributos.
+	@Value("transferIdleMusic")
+	private String transferaudio;
+	
+	//TODO En un primer momento le estamos asignando un valor hardcodeado.
+	//Está pendiente ver de qué manera toman valor estos atributos.
+	@Value("10s")
+	private String transferConnectiontimeout;
+
+	//TODO En un primer momento le estamos asignando un valor hardcodeado.
+	//Está pendiente ver de qué manera toman valor estos atributos.
+	@Value("20s")
+	private String maxtime;
 
 	public boolean isBargein() {
 		return bargein;
@@ -50,6 +65,30 @@ public class AppConfigDefaults implements Serializable {
 
 	public void setCatchHangup(boolean catchHangup) {
 		this.catchHangup = catchHangup;
+	}
+
+	public String getTransferaudio() {
+		return transferaudio;
+	}
+
+	public void setTransferaudio(String transferaudio) {
+		this.transferaudio = transferaudio;
+	}
+
+	public String getTransferConnectiontimeout() {
+		return transferConnectiontimeout;
+	}
+
+	public void setTransferConnectiontimeout(String transferConnectiontimeout) {
+		this.transferConnectiontimeout = transferConnectiontimeout;
+	}
+
+	public String getMaxtime() {
+		return maxtime;
+	}
+
+	public void setMaxtime(String maxtime) {
+		this.maxtime = maxtime;
 	}
 
 }
