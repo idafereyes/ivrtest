43e882c4a08204f42eafaf3902bf5baec05937f3
jfcorugedoIsban <juan.fernandez-corugedo@servexternos.isban.es>
2013-09-12 12:10:23 +0200

    Aplicacion de pruebas

---
+++ jvoiceframework-testapp/src/main/java/org/isb/ivr/flow/user/TransformActionMessages.java
@@ -0,0 +1,11 @@
+package org.isb.ivr.flow.user;
+
+import ch.qos.cal10n.BaseName;
+import ch.qos.cal10n.Locale;
+import ch.qos.cal10n.LocaleData;
+
+@BaseName("org.isb.ivr.flow.user.TransformActionMessages")
+@LocaleData(@Locale(value="es_ES", charset="UTF8"))
+public enum TransformActionMessages {
+	  PRUEBA;
+}
