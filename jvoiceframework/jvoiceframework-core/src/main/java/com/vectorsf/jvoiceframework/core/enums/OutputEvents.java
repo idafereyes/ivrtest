e87985848650b71c85395e90f7a719be82a6bf4b
idafereyes <idafe.reyes@servexternos.isban.es>
2013-09-19 17:46:36 +0200

    Se crea el enumeration OutputEvents.

---
+++ jvoiceframework/jvoiceframework-core/src/main/java/com/vectorsf/jvoiceframework/core/enums/OutputEvents.java
@@ -0,0 +1,14 @@
+package com.vectorsf.jvoiceframework.core.enums;
+
+/**
+ * This enumeration represents all possible events at Output element.
+ * 
+ * @author idafereyes
+ */
+
+public enum OutputEvents {
+	
+	SUCCESS,
+	HANGUP
+	
+}
