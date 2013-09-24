11c1c2fd1a3c0967d45a1d2144f051d0e61a2886
idafereyes <idafe.reyes@servexternos.isban.es>
2013-09-24 13:28:38 +0200

    Se añade enumeration RecordEvents.

---
+++ jvoiceframework/jvoiceframework-core/src/main/java/com/vectorsf/jvoiceframework/core/enums/RecordEvents.java
@@ -0,0 +1,17 @@
+package com.vectorsf.jvoiceframework.core.enums;
+
+/**
+ * This enumeration represents all possible events at Record element.
+ * 
+ * @author idafereyes
+ */
+
+public enum RecordEvents {
+	
+	RECORDED,
+	HANGUP,
+	ERROR,
+	NORESOURCE,
+	RECORDUNSUPPORTED
+	
+}
