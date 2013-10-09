5b4b1eaa8871552241833d2c166bceec7eddd158
sergiomilla <sergio.milla@servexternos.isban.es>
2013-10-09 11:31:37 +0200

    Correcciones Wellcome.java e input.xml

--- jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/locution/Wellcome.java
+++ jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/locution/Wellcome.java
@@ -4,7 +4,7 @@ import ch.qos.cal10n.Locale;
 import ch.qos.cal10n.LocaleData;
 
 @BaseName("com.vectorsf.jvoiceframework.testapp.locution.Wellcome")
-@LocaleData(defaultCharset="UTF8", value={@Locale(value="es_ES", charset="ISO8859_1"), @Locale(value="en_US")})
+@LocaleData(defaultCharset="UTF8", value={@Locale(value="es_ES", charset="ISO8859_1")})
 public enum Wellcome {
 	WELLCOME_RETAIL,
 	WELLCOME_PYME_RETAIL,
