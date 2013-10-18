91858eeb47980f51f913dcd5910644758fff5a18
idafereyes <idafe.reyes@servexternos.isban.es>
2013-10-18 13:20:12 +0200

    Mejoras en say-as en jvoice-test-app-.

--- jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/action/AccountsLocutions.java
+++ jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/action/AccountsLocutions.java
@@ -1,12 +1,17 @@
 package com.vectorsf.jvoiceframework.testapp.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
+import com.vectorsf.jvoiceframework.core.bean.AudioItem;
+import com.vectorsf.jvoiceframework.core.bean.SayAs;
+import com.vectorsf.jvoiceframework.core.bean.Wording;
+import com.vectorsf.jvoiceframework.core.enums.InterpretAs;
 import com.vectorsf.jvoiceframework.core.service.locution.LocutionProvider;
+import com.vectorsf.jvoiceframework.testapp.locution.Accounts;
 import com.vectorsf.jvoiceframework.testapp.service.portfolio.Account;
 
 @Component("accountsLocutions")
@@ -15,11 +20,11 @@ public class AccountsLocutions {
 	@Autowired
 	LocutionProvider locutionProvider;
 				 
-	public String getAccountSelectionLocution(List<Account> accounts, Enum<?> key, Enum<?> remainingKey, int offset) {
+	public List<AudioItem> getAccountSelectionLocutions(List<Account> accounts, int offset) {
 		
 		final int ACCOUNT_NUMBER = 5;
 		
-		String locution = "";		
+		List<AudioItem> locutions = new ArrayList<AudioItem>();		
 		Account account = null;
 		int index  = offset;
 		
@@ -27,16 +32,32 @@ public class AccountsLocutions {
 			
 			if (index < accounts.size()) {			
 				account = accounts.get(index);
-				locution += " " + locutionProvider.getWording(key, account.getIdEnding(), index + 1);
+				
+				AudioItem aiA = new AudioItem();
+				aiA.setWording(locutionProvider.getWording(Accounts.ACCOUNT_SELECTION_PT1_A));
+				locutions.add(aiA);
+				
+				AudioItem aiB = new AudioItem();
+				Wording wording = new Wording();
+				wording.setText(account.getIdEnding());
+				wording.setSayAs(new SayAs(InterpretAs.DIGITS));
+				aiB.setWording(wording);
+				locutions.add(aiB);
+				
+				AudioItem aiC = new AudioItem();
+				aiC.setWording(locutionProvider.getWording(Accounts.ACCOUNT_SELECTION_PT1_C, index + 1));
+				locutions.add(aiC);
 			}
 			else {
 				break;
 			}
 		}
 		if (index >= offset +  ACCOUNT_NUMBER) {
-			locution += " "  + locutionProvider.getWording(remainingKey);
+			AudioItem aiRemaining = new AudioItem();
+			aiRemaining.setWording(locutionProvider.getWording(Accounts.REMAINING_ACCOUNT_SELECTION));
+			locutions.add(aiRemaining);
 		}	
-		return locution;
+		return locutions;
 	}
 
 }
--- jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/locution/Accounts.java
+++ jvoice-test-app/src/main/java/com/vectorsf/jvoiceframework/testapp/locution/Accounts.java
@@ -6,9 +6,12 @@ import ch.qos.cal10n.LocaleData;
 @BaseName("com.vectorsf.jvoiceframework.testapp.locution.Accounts")
 @LocaleData(defaultCharset="UTF8", value={@Locale(value="es_ES", charset="ISO8859_1"), @Locale(value="en_US")})
 public enum Accounts {
-	ACCOUNT_SELECTION_PT1,
-	ACCOUNT_SELECTION_PT2,
-	ACCOUNT_SELECTION_PT3,
+	ACCOUNT_SELECTION_PT1_A,
+	ACCOUNT_SELECTION_PT1_C,
+	ACCOUNT_SELECTION_PT2_A,
+	ACCOUNT_SELECTION_PT2_C,
+	ACCOUNT_SELECTION_PT3_A,
+	ACCOUNT_SELECTION_PT3_C,
 	REMAINING_ACCOUNT_SELECTION,
 	ACCOUNT_SELECTION_NM,
 	ACCOUNT_SELECTION_NI,
