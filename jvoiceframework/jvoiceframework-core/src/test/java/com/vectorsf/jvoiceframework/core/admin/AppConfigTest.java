1f0a5cdb835b71a2287f232b3d42c7aae97cd9d5
mvinuesa <mvinuesa@servexternos.isban.es>
2013-12-19 12:07:15 +0100

    Se añade test a configuración

--- jvoiceframework/jvoiceframework-core/src/test/java/com/vectorsf/jvoiceframework/core/admin/AppConfigTest.java
+++ jvoiceframework/jvoiceframework-core/src/test/java/com/vectorsf/jvoiceframework/core/admin/AppConfigTest.java
@@ -1,24 +1,42 @@
 package com.vectorsf.jvoiceframework.core.admin;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Esta clase para probar la lectura de los ficheros de configuración
+ * Esta clase prueba la lectura de los ficheros de configuración
  * @author mvinuesa
  *
  */
 public class AppConfigTest {
     
+	ClassPathXmlApplicationContext applicationContext = null;
 
-	@Test
-	public void testConfigAudiosFormatSuffsix(){
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/vectorsf/jvoiceframework/core/admin/test-config-context.xml");
+	@Before
+	public void startContext() {
+		applicationContext = new ClassPathXmlApplicationContext("com/vectorsf/jvoiceframework/core/admin/test-config-context.xml");
 		applicationContext.refresh();
+	}
+	
+	@Test
+	public void testConfigValue(){
 		AppConfig appConfig = (AppConfig)applicationContext.getBean("app");
 		assertEquals("Must be equals",appConfig.getValue("max"),"25");
+	}
+	
+	@Test
+	public void testConfigValueNull(){
+		AppConfig appConfig = (AppConfig)applicationContext.getBean("app");
+		assertNull("Must be null",appConfig.getValue("noValue"));
+	}
+	
+	@After
+	public void stopContext() {
 		applicationContext.close();
 	}
     
