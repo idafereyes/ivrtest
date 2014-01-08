662f92645b1dd3276b7170a2ea4f723b11078b05
idafereyes <idafe.reyes@servexternos.isban.es>
2014-01-08 11:22:24 +0100

    Se cambia el nombre del fichero de config del isban-logger.

--- jvoiceframework/jvoiceframework-isban-logger/src/main/java/com/vectorsf/jvoiceframework/isban/logger/config/ConfigurationWrapper.java
+++ jvoiceframework/jvoiceframework-isban-logger/src/main/java/com/vectorsf/jvoiceframework/isban/logger/config/ConfigurationWrapper.java
@@ -16,7 +16,7 @@ import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 
 public class ConfigurationWrapper {
 	
-	private static final String COMMON_LOGGER_CONFIG_FILE = "common-logger-config.xml";
+	private static final String COMMON_LOGGER_CONFIG_FILE = "isban-logger-config.xml";
 	
 	private static final Configuration configuration;
 	
